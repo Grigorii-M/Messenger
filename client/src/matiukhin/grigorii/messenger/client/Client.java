548a4c8fe017b8c12bb950ca27a780203a670979
Grigorii-M <grigory36m@gmail.com>
2022-04-21 22:01:27 +0300

    Added gradle dependency

--- client/src/matiukhin/grigorii/messenger/client/Client.java
+++ client/src/matiukhin/grigorii/messenger/client/Client.java
@@ -4,9 +4,7 @@ import matiukhin.grigorii.messenger.client.ui.MessengerFrame;
 
 import javax.swing.*;
 import java.lang.reflect.InvocationTargetException;
-
 public class Client {
-
     public static void main(String[] args) {
         try {
             SwingUtilities.invokeAndWait(MessengerFrame::new);
